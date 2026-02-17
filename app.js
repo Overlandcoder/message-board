5a298365363eeef5db04113f0d20a3a524bb4e72
Overlander <anuragnandal@hotmail.com>
2026-02-16 21:56:11 -0500

    Use routers and views

--- app.js
+++ app.js
@@ -1,12 +1,16 @@
-const express = require("express")
-const app = express()
+const express = require("express");
+const app = express();
+const path = require("node:path");
+const indexRouter = require("./routes/indexRouter");
+const newRouter = require("./routes/newRouter");
 
-app.get("/", (req, res) => res.send("Hello"))
+app.use("/", indexRouter);
+app.use("/new", newRouter);
 
 const PORT = 3000;
-app.listen(PORT, (error) => {
-  if (error) {
-    throw error
-  }
-  console.log(`Express app - listening on port ${PORT}`)
-})
+app.listen(PORT, () => {
+  console.log(`Express app - listening on port ${PORT}`);
+});
+
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
